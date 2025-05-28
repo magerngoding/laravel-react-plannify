02c87162c449becb1b69a7de97191bd78ee72bb8
Hafizh <Hafizh@gmail.com>
2025-05-28 12:25:15 +0700

    Install shadcnUI, setup configuration, component based

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const STATUS = {
+  TODO: 'To Do',
+  INPROGRESS: 'In Progress',
+  ONREVIEW: 'On Review',
+  UNKNOWN: 'Unknown'
+};
+
+export const PRIORITY = {
+  URGENT: 'Urgent',
+  HIGH: 'high',
+  MEDIUM: 'Medium',
+  LOW: 'Low',
+  UNKNOWN: 'Unknown'
+};
