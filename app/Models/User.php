5a7f08dcfef013b661d971ffacf4f5c15aaf4e36
Hafizh <Hafizh@gmail.com>
2025-05-29 12:03:48 +0700

    Analisis flow project, create model migration User & Workspace

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,32 +11,22 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    // $guarded kebalikan nya yang tidak boleh diisi
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'username',
+        'avatar',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
